afc5e127b9c0736812368576092a7a62aa32fb57
zhangmk <51670196@qq.com>
2019-03-15 14:36:36 +0800

    init

---
+++ 1006_clumsy_mid_test.go
@@ -0,0 +1,24 @@
+package leetgo
+
+import "testing"
+
+func Test1006(t *testing.T) {
+	cases := []struct {
+		in, want int
+	}{
+		{100, 101},
+		{10, 12},
+		{1, 1},
+		{2, 2},
+		{3, 6},
+		{4, 7},
+	}
+
+	for _, c := range cases {
+		got := clumsy(c.in)
+		if got != c.want {
+			t.Errorf("clumsy(%v)==%v, want %v", c.in, got, c.want)
+		}
+
+	}
+}

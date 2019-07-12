d65bb80fd361e5004658817778873deeedc5459e
zhangmk <51670196@qq.com>
2019-07-12 10:25:17 +0800

    443

--- 443_stringcompress_easy/443_stringcompress_easy.go
+++ 443_stringcompress_easy/443_stringcompress_easy.go
@@ -2,6 +2,21 @@ package leetgo
 
 import "strconv"
 
+/*
+给定一组字符，使用原地算法将其压缩。
+
+压缩后的长度必须始终小于或等于原数组长度。
+
+数组的每个元素应该是长度为1 的字符（不是 int 整数类型）。
+
+在完成原地修改输入数组后，返回数组的新长度。
+
+
+
+进阶：
+你能否仅使用O(1) 空间解决问题？
+*/
+
 func compress(chars []byte) int {
 	t, i := 0, 0
 	for t < len(chars) && i < len(chars) {

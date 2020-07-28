589a17f82e0af6b7c283931da5ea9442b459d12c
nocmk2 <51670196@qq.com>
2020-07-28 19:00:08 +0800

    head tail

--- lru/lru.go
+++ lru/lru.go
@@ -1,18 +1,26 @@
 package leetgo
 
+// https://leetcode-cn.com/problems/lru-cache-lcci/solution/goshuang-xiang-lian-biao-map-shi-xian-lru-by-pengt/
+
 /*LinkNode for Doubly Linked List*/
 type LinkNode struct {
 	key, value int
-	prev, next *LinkNode
+	pre, next  *LinkNode
 }
 
 /*LRUCache implements lru */
 type LRUCache struct {
-	m map[int]*LinkNode
+	m          map[int]*LinkNode
+	capacity   int
+	head, tail *LinkNode
 }
 
 /*Constructor new */
 func Constructor(capacity int) LRUCache {
+	head := &LinkNode{-1, -1, nil, nil}
+	tail := &LinkNode{-1, -1, nil, nil}
+	head.next = tail
+	tail.pre = head
 
 	return LRUCache{}
 }
